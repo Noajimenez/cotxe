d08f5b06eeb7663772865fa111c1464d50bd522b
Noajimenez <82875952+Noajimenez@users.noreply.github.com>
2021-05-27 14:21:42 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,4 @@
 basic.forever(function () {
-    if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
-        cuteBot.motors(80, 80)
-    } else if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
-        cuteBot.motors(80, 20)
-    } else if (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
-        cuteBot.motors(20, 80)
-    }
-})
-basic.forever(function () {
     basic.showLeds(`
         # . . . #
         # # . . #
@@ -16,4 +7,18 @@ basic.forever(function () {
         # . . . #
         `)
     cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x7f00ff)
+})
+basic.forever(function () {
+    if (cuteBot.tracking(cuteBot.TrackingState.L_R_line)) {
+        cuteBot.motors(80, 80)
+        cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x7f00ff)
+    } else if (cuteBot.tracking(cuteBot.TrackingState.L_unline_R_line)) {
+        cuteBot.motors(80, 20)
+        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0xff0000)
+        cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0x00ff00)
+    } else if (cuteBot.tracking(cuteBot.TrackingState.L_line_R_unline)) {
+        cuteBot.motors(20, 80)
+        cuteBot.colorLight(cuteBot.RGBLights.RGB_R, 0xff0000)
+        cuteBot.colorLight(cuteBot.RGBLights.RGB_L, 0x00ff00)
+    }
 })
